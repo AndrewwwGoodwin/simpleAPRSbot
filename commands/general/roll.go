3e25170c0f699bc16b767027f17fce563f8c611b
Andrew Goodwin <pgoodwi3@students.kennesaw.edu>
2024-08-14 10:04:47 -0400

    feature idea comment

--- commands/general/roll.go
+++ commands/general/roll.go
@@ -8,6 +8,9 @@ import (
 )
 
 func Roll(args []string, f aprs.Frame) {
+	// take in 1 arg that would allow for setting max roll
+	// otherwise default to rolling out of 100
+
 	aprsHelper.AprsTextReply(strconv.Itoa(rand.IntN(100)), f)
 	return
 }

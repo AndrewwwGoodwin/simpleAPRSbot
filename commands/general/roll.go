af13d363eb136d0f7cf0486ccc6009cc4b0afe89
Andrew Goodwin <pgoodwi3@students.kennesaw.edu>
2024-08-16 12:44:42 -0400

    upgrade !roll to support user-inputted maximum

--- commands/general/roll.go
+++ commands/general/roll.go
@@ -10,7 +10,15 @@ import (
 func Roll(args []string, f aprs.Frame) {
 	// take in 1 arg that would allow for setting max roll
 	// otherwise default to rolling out of 100
-
-	aprsHelper.AprsTextReply(strconv.Itoa(rand.IntN(100)), f)
+	var maxRoll = 100
+	if len(args) > 0 {
+		var err error
+		maxRoll, err = strconv.Atoi(args[0])
+		if err != nil || maxRoll <= 0 {
+			// Handle error or invalid maxRoll value, default to 100
+			maxRoll = 100
+		}
+	}
+	aprsHelper.AprsTextReply(strconv.Itoa(rand.IntN(maxRoll)), f)
 	return
 }

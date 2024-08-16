9c857c054d5e74ab453d4cacadfcf78a7ef6b8d8
Andrew Goodwin <pgoodwi3@students.kennesaw.edu>
2024-08-16 12:35:03 -0400

    flesh out time a bit for allowing specification of time zones

--- commands/general/time.go
+++ commands/general/time.go
@@ -1,14 +1,54 @@
 package general
 
 import (
+	"fmt"
 	"github.com/ebarkie/aprs"
 	"simpleAPRSbot-go/aprsHelper"
+	"strings"
 	"time"
 )
 
-//add timezone support via args
+// add timezone support via args
+var timezoneAbbrs = map[string]string{
+	"UTC": "UTC",
+	"EST": "America/New_York",
+	"EDT": "America/New_York",
+	"CST": "America/Chicago",
+	"CDT": "America/Chicago",
+	"MST": "America/Denver",
+	"MDT": "America/Denver",
+	"PST": "America/Los_Angeles",
+	"PDT": "America/Los_Angeles",
+}
 
 func Time(args []string, f aprs.Frame) {
-	aprsHelper.AprsTextReply(time.Now().UTC().Format("02 Jan 06 15:04:05 MST"), f)
-	return
+	// Default timezone to UTC
+	location := "UTC"
+
+	// If arguments are provided, use the first one as the timezone
+	if len(args) > 0 {
+		arg := strings.ToUpper(args[0])
+		if mappedLocation, exists := timezoneAbbrs[arg]; exists {
+			location = mappedLocation
+		} else {
+			// Handle error if the timezone abbreviation is invalid
+			aprsHelper.AprsTextReply(fmt.Sprintf("Error: Invalid timezone abbreviation %s", arg), f)
+			return
+		}
+	}
+
+	// Parse the timezone
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		// Handle error if the timezone is invalid
+		aprsHelper.AprsTextReply(fmt.Sprintf("Error: Invalid timezone %s", location), f)
+		return
+	}
+
+	// Get the current time in the specified location
+	currentTime := time.Now().In(loc)
+	formattedTime := currentTime.Format("02 Jan 06 15:04:05 MST")
+
+	// Send the formatted time as a reply
+	aprsHelper.AprsTextReply(formattedTime, f)
 }
